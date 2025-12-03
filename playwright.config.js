7a8ce19c24369bb51c8e9784fc82ff7bb9be4a42
p-flamer <paula.flamer@gmail.com>
2025-12-03 10:34:42 +0100

    test: split projects

--- playwright.config.js
+++ playwright.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://mad-qa.pl/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -36,19 +36,40 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'desktop',
+      use: { 
+        ...devices['Desktop Chrome']
+      },
     },
-
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: 'mobile',
+      use: { 
+        ...devices['Pixel 5']
+      },
     },
+    // {
+    //   name: 'codeme',
+    //   use: { 
+    //     ...devices['Desktop Chrome'],
+    //     baseURL: 'https://codeme.pl/' 
+    //   },
+    // },
+    // {
+    //   name: 'tydzienprogramisty',
+    //   use: {
+    //     ...devices['Pixel 5'],
+    //     baseURL: 'https://tydzienprogramisty.pl/'
+    //   },
+    // },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
